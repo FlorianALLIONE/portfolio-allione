e335df398ac4398c6ba2895b9d28737f650b904c
Florian Allione <florian.allione@ynov.com>
2021-08-22 18:09:15 +0200

    Header, footer et a propos

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    $(function () {
+      $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 10) {
+          $('.navbar').addClass('active');
+        } else {
+          $('.navbar').removeClass('active');
+        }
+      });
+    });
+  }
+
+}
